782bf1509e0bf4ab5491eaf79508dbf9678d8e9b
vicek <vicek.skora@laposte.net>
2018-01-12 15:23:23 +0100

    categorie list item layout

--- app/src/main/java/com/iut/guarssif3u/boutique/SalesActivity.java
+++ app/src/main/java/com/iut/guarssif3u/boutique/SalesActivity.java
@@ -2,6 +2,7 @@ package com.iut.guarssif3u.boutique;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SalesActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class SalesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/iut/guarssif3u/boutique/ShopActivity.java
+++ app/src/main/java/com/iut/guarssif3u/boutique/ShopActivity.java
@@ -2,6 +2,7 @@ package com.iut.guarssif3u.boutique;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ShopActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
