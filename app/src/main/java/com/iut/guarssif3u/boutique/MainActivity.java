41d3e7672aa09878bb0033d4fe48755e7b0d3fa8
vicek <vicek.skora@laposte.net>
2018-01-12 14:48:44 +0100

    Ajout layout sales + shop

--- app/src/main/java/com/iut/guarssif3u/boutique/MainActivity.java
+++ app/src/main/java/com/iut/guarssif3u/boutique/MainActivity.java
@@ -1,5 +1,6 @@
 package com.iut.guarssif3u.boutique;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,18 +81,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_shop) {
+            Intent intent = new Intent(this, ShopActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_sales) {
+            Intent intent = new Intent(this, SalesActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
