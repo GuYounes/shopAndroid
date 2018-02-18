c3339d98e2ec9ad88beec858ce73b7471caa135b
vicek <vicek.skora@laposte.net>
2018-02-18 10:59:57 +0100

    bind categorie btn

--- app/src/main/java/com/iut/guarssif3u/boutique/adapter/CategorieAdapter.java
+++ app/src/main/java/com/iut/guarssif3u/boutique/adapter/CategorieAdapter.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -77,4 +78,5 @@ public class CategorieAdapter extends ArrayAdapter<Categorie> {
 
         return convertView;
     }
+
 }
